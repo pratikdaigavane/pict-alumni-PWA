07783315f28d7289416ba99890f32d553c14c4cb
pratikdaigavane <daigavanep@gmail.com>
2019-12-22 19:00:31 +0530

    service worker and manifest added

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/main.js'
+];
+
+// install event
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    //console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request)
+        })
+    )
+});
