f86102b202b25183c5a7a241a20e2ab7155f044e
pratikdaigavane <daigavanep@gmail.com>
2019-12-22 18:51:43 +0530

    cloud function added

---
+++ public/main.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+    $('#send').submit((e)=>{
+        e.preventDefault();
+        var message = $('#message').val();
+        $.ajax({
+            url: 'https://us-central1-pict-alumni.cloudfunctions.net/messageMe',
+            method: "post",
+            crossDomain: true,
+            crossOrigin: true,
+            async: false,
+            contentType: "application/json",
+            data: JSON.stringify({
+                text: message
+            }),
+            success: function (res) {
+                M.toast({html: res.message.text});
+            },
+            error: function (err) {
+                alert(err);
+            }
+        })
+    })
+
+});
