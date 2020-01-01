09872e1b8075aa1c838bc68e1aa788cb189476cf
pratikdaigavane <daigavanep@gmail.com>
2020-01-01 11:06:07 +0530

    Home page modified

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ function getFormData($form, recaptcha) {
 }
 
 $(document).ready(() => {
-
+    $('.collapsible').collapsible();
     var token;
     grecaptcha.ready(function () {
         grecaptcha.execute('6LehkMoUAAAAAEN6oCECJe5KtV_zU3U20_cpAMMt', {action: 'homepage'}).then(function (t) {
